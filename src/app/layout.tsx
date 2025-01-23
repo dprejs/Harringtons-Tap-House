7538cb61df8ae7b26874611c36bf094089e61178
Daniel Prejs <danielnprejs@gmail.com>
2025-01-23 13:11:02 -0700

    added website screen shots

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,10 @@ import Head from "next/head";
 import Image from "next/image";
 import "./App.css";
 import Link from "next/link";
-import React from "react";
+import React, { useContext } from "react";
 import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
 import { createTheme, SwipeableDrawer, ThemeProvider } from "@mui/material";
-import ContextProvider from "./context";
+import ContextProvider, { ScreenContext } from "./context";
 
 export default function RootLayout({
   children,
@@ -31,6 +31,7 @@ export default function RootLayout({
 
     }
   });
+  const screen = useContext(ScreenContext);
   return (
     <html lang="en" suppressHydrationWarning={true} className="height-100 width-100" style={{overflowX: "hidden"}}>
       <Head>
@@ -62,7 +63,7 @@ export default function RootLayout({
                 onClose={() => toggleDrawer(false)}
                 onOpen={() => toggleDrawer(true)}
               >
-                <div className="drawer">
+                <div className="drawer" >
                   <Link href="/">
                     Home
                   </Link>
